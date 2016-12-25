ee4cb33039284761267b864dafcb07472130a1ae
Michael Aduayi <Mike.Aduayi@gmail.com>
2016-12-25 01:21:04 -0500

    complted reveal behaviour w/ waypoint tech

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from "./modules/RevealOnScroll"
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+// feature-item
+new RevealOnScroll($('.feature-item'),"85%");
+// tetimonials
+new RevealOnScroll($(".testimonial"),"65%");
